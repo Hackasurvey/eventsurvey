5f0297bfe113a2126c1eecbb381caf289d61ef93
Jesse van Leth <jevanlet@microsoft.com>
2015-10-20 17:47:58 +0200

    rem old aspnet45proj

--- Eventwebapp/myeventsurvey/myeventsurvey/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace myeventsurvey.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Eventwebapp/myeventsurvey/myeventsurvey/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace myeventsurvey.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
