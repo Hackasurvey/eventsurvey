ee5d138803f642893a07f3f9c0d7da642e5113fe
Anders Lybecker <anderslybecker@hotmail.com>
2015-10-20 23:06:44 +0200

    ...

--- mysurveyaspnet4/mysurveyaspnet4/WepAppSlotSwapIfAllGood.ps1
+++ mysurveyaspnet4/mysurveyaspnet4/WepAppSlotSwapIfAllGood.ps1
@@ -1,4 +1,4 @@
-$r = Invoke-WebRequest -Uri http://hackasurvey.azurewebsites.net -Method POST
+$r = Invoke-WebRequest -Uri http://hackasurvey-staging.azurewebsites.net -Method POST
 
 if ($r.StatusCode –eq 200)
 {
